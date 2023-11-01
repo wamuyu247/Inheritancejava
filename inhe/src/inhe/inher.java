60c0cf26576eb0f536d0d4cd18fdf972456bc186
wamuyu247 <95418608+wamuyu247@users.noreply.github.com>
2023-11-01 09:42:51 +0300

    Add files via upload

--- inhe/src/inhe/inher.java
+++ inhe/src/inhe/inher.java
@@ -1,10 +1,10 @@
 package inhe;
 class BankAccount{
-	int acNo,acPhone;
-	String acName,acEmail;
+	int acNo;
+	String acName,acEmail,acPhone;
 	double AcBal;
 	
-	public BankAccount(int acNO, String acName,int acPhone,String acEmail,double AcBal)
+	public BankAccount(int acNO, String acName,String acPhone,String acEmail,double AcBal)
 	{
 		this.acNo=acNo;
 		this.acName=acName;
@@ -13,7 +13,7 @@ class BankAccount{
 		this.AcBal=AcBal;
 	}
 	
-	public void accountHolder() {
+	public void accountHolder (){
 		System.out.println("Account Number:"+acNo);
 		System.out.println("Account Number:"+acName);
 		System.out.println("Account Number:"+acPhone);
@@ -27,12 +27,34 @@ class BankAccount{
 		AcBal+=amount;
 		System.out.println("Deposited:"+amount);
 	}
+	public void displayBalance() {
+		System.out.println("Account Number:"+acNo+"Balance"+AcBal);
+	}
+}
+class SavingsAccount extends BankAccount {
+    public SavingsAccount(int acNo, String acName, String acPhone, String acEmail, double AcBal) {
+        super(acNo, acName, acPhone, acEmail, AcBal);
+    }
 }
 
+class CurrentAccount extends BankAccount {
+    public CurrentAccount(int acNo, String acName, String acPhone, String acEmail, double AcBal) {
+        super(acNo, acName, acPhone, acEmail, AcBal);
+    }
 public class inher {
 
 	public static void main(String[] args) {
-		BankAccount c=new BankAccount(0111,"FAITH",0111,"faith@",1000);
+		 System.out.println("SAVING ACCOUNT DETAILS");
+         SavingsAccount sac = new SavingsAccount(1, "Ann Kamau", "0711100011", "faith@wamuyu", 1000);
+         sac.accountHolder();
+         sac.deposit(500);
+         sac.displayBalance();
+
+         System.out.println("\nCURRENT ACCOUNT DETAILS");
+         CurrentAccount cac = new CurrentAccount(2, "Favour", "074466667", "faith@wamuyu", 100);
+         cac.accountHolder();
+         cac.deposit(300);
+         cac.displayBalance();
 		
 	}
 
